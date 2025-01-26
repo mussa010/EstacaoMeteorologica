8bde5280a0916a267324d9effa04177bb7e4e9bc
Matheus Teixeira Mussalam <98722373+mussa010@users.noreply.github.com>
2025-01-26 15:42:53 -0300

    EstacaoMeteorologica

---
+++ estacao/credenciais.h
@@ -0,0 +1,11 @@
+#ifndef CREDENCIAIS_H
+#define CREDENCIAIS_H
+
+const char* ssid = "mussa";
+const char* password = "A1302701";
+const char* chave_api = "AIzaSyAOACb4RdUWDN8OsGzBeOMRV36L7WpbldA";
+const char* email = "matheus.mussalam@sou.unaerp.edu.br";
+const char* senhaEmail = "1234567890";
+const char* url = "https://estacao-geometrica-esp32-default-rtdb.firebaseio.com/";
+
+#endif // CREDENCIAIS_H
